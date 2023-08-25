c68634b66b7d109bc916741a787fefc7958f5c2d
GustaCS <gustavocorsantanna@hotmail.com>
2023-08-25 09:39:39 -0300

    mudanças aula

--- site_marvel_fiap/src/app/page.js
+++ site_marvel_fiap/src/app/page.js
@@ -1,28 +1,55 @@
 import Campo_card from "@/components/Campo_card";
+import Card_herois from "@/components/Cards_herois";
 import Titulo from "@/components/Titulo";
-import Card_herois from "@/components/cards_herois";
 
 export default function Home() {
+
+  const marvel =[
+    {
+      nome:"homem Aranha",
+      foto:"https://portalpopline.com.br/wp-content/uploads/2023/06/homem-aranha-4-2025-758x570.jpg"
+    },
+    {
+      nome:"Captão America",
+      foto:"https://portalpopline.com.br/wp-content/uploads/2023/06/homem-aranha-4-2025-758x570.jpg"
+    },
+    {
+      nome:"Thanos",
+      foto:"https://portalpopline.com.br/wp-content/uploads/2023/06/homem-aranha-4-2025-758x570.jpg"
+    },
+    {
+      nome:"Hulk",
+      foto:"https://portalpopline.com.br/wp-content/uploads/2023/06/homem-aranha-4-2025-758x570.jpg"
+    },
+    {
+      nome:"Pantera Negra",
+      foto:"https://portalpopline.com.br/wp-content/uploads/2023/06/homem-aranha-4-2025-758x570.jpg"
+    }
+  ]
+
   return ( //JSX
     <main>
-      <div className="Fundo w-full h-screen bg-black">
-      <div className="RectangleInicial w-screen h-24 bg-red-800 bg-opacity-50">
-        <h1 className="text-zinc-50 text-6xl p-4 float-left">Marvel</h1>
-        <a href="#" className="text-zinc-50 text-3xl ml-3 p-7 float-left">Herois</a>
-        <a href="#" className="text-zinc-50 text-3xl ml-3 p-7 float-left">Vilões</a>
-        <a href="#" className="text-zinc-50 text-3xl ml-3 p-7 float-left">Equipes</a>
-        <input type="text" className="m-6 p-3 rounded-md ml-80 w-1/4"></input>
+      <div className="absolute">
+        <img src="/image/fundo_marvel.png" className="w-screen h-screen"/>
       </div>
+      <div className=" w-full h-screen bg-black bg-opacity-50 blur-1 absolute">
+        {/* menu */}
+      <div className="w-screen h-20 bg-red-800 bg-opacity-50">
+        <h1 className="text-zinc-100 border-solid text-4xl p-4 float-left">MARVEL</h1>
+        <ul className="flex pt-5 pl-32 float-left">
+          <li className="text-zinc-100 text-3xl ml-3"><a href="#">Herois</a></li>
+          <li className="text-zinc-100 text-3xl ml-3"><a href="#">Vilões</a></li>
+          <li className="text-zinc-100 text-3xl ml-3"><a href="#">Equipes</a></li>
+        </ul>
+        <input type="text" className="m-5 p-2 ml-60 rounded-md  w-1/4"></input>
+      </div>
+      {/*personagens*/}
       <Titulo>Personagens</Titulo>
       <Campo_card>
-      <Card_herois>Captão America</Card_herois>
-        <Card_herois>Captão America</Card_herois>
-        <Card_herois>Homem Aranha</Card_herois>
-        <Card_herois>Homem Aranha</Card_herois>
-        <Card_herois>Homem Aranha</Card_herois>
-        <Card_herois>Homem Aranha</Card_herois>
-        </Campo_card>
+        {marvel.map(herois=> <Card_herois heroi={herois}/>)}
+      </Campo_card>
       </div>
+      
     </main>
    
   )
--- site_marvel_fiap/src/components/Cards_herois.jsx
+++ site_marvel_fiap/src/components/Cards_herois.jsx
@@ -1,9 +1,9 @@
-export default function Card_herois({children}){
+export default function Card_herois({heroi}){
 
     return(
-        <div className="w-56 h-72 ml-5">
-        <img className="Rectangle7 w-56 h-[255px]" src="https://via.placeholder.com/228x294" />
-            <h2 className="text-3xl text-center text-slate-50">{children}</h2>
+        <div className="items-center ml-4 gap-1">
+        <img className="w-56 h-60 " src={heroi.foto} />
+            <h2 className="text-3xl text-center text-slate-50">{heroi.nome}</h2>
         </div>
     )
 
